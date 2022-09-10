a053d533d925d7e60dcdd6452324aacc1f2604d8
luanrz <luanrz@qq.com>
2022-09-10 14:11:19 +0800

    nacos discovery

---
+++ nacos-consumer/src/main/java/com/example/nacosconsumer/NacosConsumerApplication.java
@@ -0,0 +1,54 @@
+package com.example.nacosconsumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+@EnableDiscoveryClient(autoRegister = false)
+public class NacosConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConsumerApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @RestController
+    class HelloController {
+        @Autowired
+        private DiscoveryClient discoveryClient;
+
+        @Autowired
+        private RestTemplate restTemplate;
+
+        private String serviceName = "nacos-provider";
+
+        @GetMapping("/info")
+        public String info() {
+            String serviceInfo = discoveryClient.getServices().toString();
+            String instanceInfo = discoveryClient.getInstances(serviceName).stream().map(instance -> instance.getInstanceId() + "," + instance.getServiceId() + "," + instance.getHost() + "," + instance.getPort() + "<br>").collect(Collectors.joining());
+            return serviceInfo + "<br><br>" + instanceInfo;
+        }
+
+        @GetMapping("/hello")
+        public String hello() {
+            List<ServiceInstance> instances = discoveryClient.getInstances(serviceName);
+            ServiceInstance serviceInstance = instances.stream().findAny().orElseThrow(() -> new IllegalStateException("no " + serviceName + " instance available"));
+            return restTemplate.getForObject("http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/echo?name=nacos",String.class);
+        }
+    }
+}
---
+++ nacos-provider/src/main/java/com/example/nacosprovider/NacosProviderApplication.java
@@ -0,0 +1,25 @@
+package com.example.nacosprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@SpringBootApplication
+public class NacosProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderApplication.class, args);
+    }
+
+    @RestController
+    class EchoController {
+        @GetMapping("/echo")
+        public String echo(HttpServletRequest request) {
+            return "echo:" + request.getParameter("name");
+        }
+    }
+
+}
