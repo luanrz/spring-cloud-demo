b4cc82eed178f2633583e36b419cdebd85321fcc
luanrz <luanrz@qq.com>
2022-09-23 06:38:47 +0800

    refactor project structure

---
+++ service-registry-discovery/eureka-provider/src/main/java/org/example/eurekaprovider/EurekaProviderApplication.java
@@ -0,0 +1,37 @@
+package org.example.eurekaprovider;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+public class EurekaProviderApplication {
+
+    @Autowired
+    Environment environment;
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaProviderApplication.class, args);
+    }
+
+    @RestController
+    class EchoController {
+        @GetMapping("/echo")
+        public String echo(HttpServletRequest request) {
+            return "echo:" + request.getParameter("name");
+        }
+
+        @GetMapping("/serverIpPort")
+        public String info() throws UnknownHostException {
+            return InetAddress.getLocalHost().getHostAddress() + ":" + environment.getProperty("server.port");
+        }
+    }
+
+}
