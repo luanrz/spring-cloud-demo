10dd4d4eb2abbd30708449f3295b4532945b0a60
luanrz <luanrz@qq.com>
2023-06-10 14:59:47 +0800

    gateway

---
+++ gateway/spring-cloud-gateway/src/main/java/org/example/AddResponseHeaderGlobalFilter.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 全局过滤器
+ */
+@Component
+@Order(-1)
+public class AddResponseHeaderGlobalFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.getHeaders().set("X-Response-Global", "Global");
+        return chain.filter(exchange);
+    }
+}
